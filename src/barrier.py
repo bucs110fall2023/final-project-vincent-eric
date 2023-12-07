4da1a2f0e73c3a6afc2058160a8666e93f5f236f
Ace-of-Spades51 <143812804+Ace-of-Spades51@users.noreply.github.com>
2023-12-06 21:35:31 -0500

    jump done, a little buggy, but done

--- src/barrier.py
+++
@@ -1,4 +0,0 @@
-class Barrier:
-    pass
-
-## Barrier to prevent players from getting off screen (this includes ground and sides) (will have to be sprite)
--- src/character.py
+++ src/character.py
@@ -6,6 +6,26 @@ class Character(pygame.sprite.Sprite):
         self.display = display
         self.x = x
         self.y = y
+        
+        # Physics Data
+        self.x_velocity = 3
+        self.y_velocity = 1.5
+        self.gravity = 0.5
+        self.jump_height = 15
+        
+        # Movement States
+        self.run_left = False
+        self.run_right = False
+        self.jump = False
+        self.press = False
+        
+        # Invisible Wall
+        self.x_min = 0
+        self.x_max = self.display.get_width()
+        self.y_min = 0
+        self.y_max = 450
+        
+        
         self.rect = pygame.Rect((x, y, 80, 100))
 
 ## character class (will have to be sprite) for both players
@@ -16,34 +36,46 @@ class Character(pygame.sprite.Sprite):
         pygame.draw.rect(self.display, (255, 0, 0), self.rect)
     
     def move(self, left, right, up): #left, right, jump are pygame inputs
-        x_speed = 3
-        y_speed = 2
-        gravity = 1
-        y_max = 30
-        
-        run_left = False
-        run_right = False
-        jump = False
-        
-        
+ 
+        # Move and Jump
         keypress = pygame.key.get_pressed()
         if keypress[left] == True:
-            run_left = True
+            self.run_left = True
+        else:
+            self.run_left = False
         if keypress[right] == True:
-            run_right = True
-        if keypress[up] == True:
-            jump = True
+            self.run_right = True
+        else:
+            self.run_right = False
+        if self.jump == False:
+            if keypress[up] == True:
+                self.jump = True
+
+
+        if self.run_left == True:
+            self.rect.x = self.rect.x - self.x_velocity
+            
+        if self.run_right == True:
+            self.rect.x = self.rect.x + self.x_velocity
         
-        if run_left == True:
-            self.rect.x = self.rect.x - x_speed
-        if run_right == True:
-            self.rect.x = self.rect.x + x_speed
-        if jump == True:
-            self.rect.y = self.rect.y - y_speed
-            y_speed = y_speed - gravity
-            if y_speed < y_max:
-                jump = False
-                y_speed = y_max
+        if self.jump == True:
+            self.rect.y =  self.rect.y - self.y_velocity
+            self.y_velocity = self.y_velocity - self.gravity
+            if self.y_velocity < -self.jump_height:
+                self.y_velocity = self.jump_height
+                self.jump = False
+                 
+                
+        # Barrier
+               
+        if self.rect.left < self.x_min:
+            self.rect.left = self.x_min
+        if self.rect.right > self.x_max:
+            self.rect.right = self.x_max
+        if self.rect.top < self.y_min:
+            self.rect.top = self.y_min
+        if self.rect.bottom > self.y_max:
+            self.rect.bottom = self.y_max
 
         
         
