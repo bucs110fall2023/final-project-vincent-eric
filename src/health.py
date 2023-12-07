e77f676de9d7697b31ec2f05b9ca06af7385b60d
VincentYou <vincentyou2005@gmail.com>
2023-12-06 23:40:08 -0500

    oh no no no

--- src/health.py
+++ src/health.py
@@ -8,10 +8,12 @@ class Health:
     def __init__(self,x,y):
         self.health = 100
 
-def health_bar(self,x,y):
-    pygame.draw.rect(self.display , YELLOW , (x, y, 400, 30))
+    def health_bar(self,x,y):
+        pygame.draw.rect(self.display , YELLOW , (x, y, 400, 30))
     
-    
-health_bar(self.p1.health, 20,20)
-health_bar(self.p2.health, 500,20)
+    fighter_1 = self.p1
+    fighter_2 = self.p2
+
+    health_bar(fighter_1.health, 20,20)
+    health_bar(fighter_2.health, 500,20)
 ## health bar
