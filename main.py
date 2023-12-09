6926f2c4b466b34ed8addabee99a5193b282f4a8
Ace-of-Spades51 <143812804+Ace-of-Spades51@users.noreply.github.com>
2023-12-08 22:27:44 -0500

    main.py removed text

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ from src.controller import Controller
 def main():
     controller = Controller()
     controller.mainloop()
-    #Create an instance on your controller object
-    #Call your mainloop
     
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
-
-# https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
     main()
