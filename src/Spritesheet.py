bc80d68e4f2978f74b1fef913267db794a78711c
VincentYou <vincentyou2005@gmail.com>
2023-12-08 23:18:38 -0500

    check check, push

--- src/Spritesheet.py
+++ src/Spritesheet.py
@@ -19,10 +19,11 @@ class SpriteSheet():
                         image.set_colorkey("color")
 
                 return image
-        
-    def get_images(self, crop_pos = (0,0)):
+            
+	def get_images(self, crop_pos =(0,0)):
 		image_list = []
   
 		for i in range(self.count):
 			image_list.append(self.get_image(i))
 		return image_list
+	1
